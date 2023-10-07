db782fa39798b20c6d29286d4031d9ba0ef7a83e
caffeinebounce <mudflat_zesty0c@icloud.com>
2023-10-06 23:07:47 -0400

    awareness view change

--- funnel/awareness.sql
+++ funnel/awareness.sql
@@ -1,9 +1,5 @@
-create database funnel;
-
-create schema awareness;
-
 create or replace view
-    impressions_clicks_spend as
+    basic_awareness as
 with
     dates as (
         select
@@ -37,11 +33,13 @@ with
     amazon_ads as (
         select
             date as date_day,
-            impressions,
-            clicks,
-            cost as spend
+            sum(impressions) as impressions,
+            sum(clicks) as clicks,
+            sum(cost) as spend
         from
             pc_fivetran_db.s2s_amazo_ads.ad_group_level_report
+        group by
+            date
     )
 select
     d.date as date,
@@ -59,13 +57,31 @@ select
     1000 * ga.spend / nullif(ga.impressions, 0) as google_ad_cpm,
     1000 * fb.spend / nullif(fb.impressions, 0) as facebook_ad_cpm,
     1000 * az.spend / nullif(az.impressions, 0) as amazon_ad_cpm,
-    ga.clicks / nullif(ga.impressions, 0) as google_ad_ctr,
-    fb.clicks / nullif(fb.impressions, 0) as facebook_ad_ctr,
-    ig.clicks / nullif(ig.impressions, 0) as instagram_ctr,
-    az.clicks / nullif(az.impressions, 0) as amazon_ctr
+    ga.clicks / nullif(ga.impressions, 0) * 100 as google_ad_ctr,
+    fb.clicks / nullif(fb.impressions, 0) * 100 as facebook_ad_ctr,
+    ig.clicks / nullif(ig.impressions, 0) * 100 as instagram_ctr,
+    az.clicks / nullif(az.impressions, 0) * 100 as amazon_ctr
 from
     dates d
     left join google_ads ga on d.date = ga.date_day
     left join facebook_ads fb on d.date = fb.date_day
     left join instagram ig on d.date = ig.date
     left join amazon_ads az on d.date = az.date_day;
+
+CREATE
+OR REPLACE PROCEDURE refresh_impressions_clicks_spend () RETURNS STRING LANGUAGE JAVASCRIPT AS $$
+var sql_command = `
+    DELETE FROM impressions_clicks_spend;
+    INSERT INTO impressions_clicks_spend SELECT * FROM q_impressions_clicks_spend;
+`;
+var statement = snowflake.createStatement({sqlText: sql_command});
+statement.execute();
+return 'Table refreshed successfully.';
+$$;
+
+CREATE TASK refresh_impressions_clicks_spend WAREHOUSE = compute_wh SCHEDULE = '720 minutes' AS
+CALL refresh_impressions_clicks_spend ();
+
+alter task refresh_impressions_clicks_spend resume;
+
+CALL refresh_impressions_clicks_spend ();
